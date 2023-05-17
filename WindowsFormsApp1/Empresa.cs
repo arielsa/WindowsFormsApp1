49a42d0069ec55951cecf023fd8ae29c3c6804b8
ariel <santa.cruz.unla@gmail.com>
2023-05-16 22:58:02 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Empresa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Empresa
+    {
+        List<Auto> la;
+        List<Persona> lp;
+        public Empresa()
+        {
+            la = new List<Auto> () ; lp = new List<Persona>();
+        }
+        public void AgregarPersona (Persona pPersona)
+        {
+            lp.Add( new Persona (pPersona) );
+        }
+
+        public object RetornaListaPersona()
+        {
+            return lp;
+        }
+    }
+}
