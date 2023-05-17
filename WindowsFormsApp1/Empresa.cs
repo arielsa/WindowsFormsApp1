65da1eda54a24310a97aa823d72a5aac187d4a9e
ariel <santa.cruz.unla@gmail.com>
2023-05-16 22:58:02 -0300

    Agregar archivos de proyecto.

---
+++ WindowsFormsApp1/Empresa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Empresa
+    {
+        List<Auto> la;
+        List<Persona> lp;
+        public Empresa()
+        {
+            la = new List<Auto> () ; lp = new List<Persona>();
+        }
+        public void AgregarPersona (Persona pPersona)
+        {
+            lp.Add( new Persona (pPersona) );
+        }
+
+        public object RetornaListaPersona()
+        {
+            var query = (from p in lp
+                        select new
+                        {
+                            DNI = p.DNI,
+                            apellido_y_nombre = $"{p.Apellido},{p.Nombre}"
+                        }).ToArray();
+
+            return query;
+        }
+    }
+}
---
+++ WindowsFormsApp1/Persona.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    public class Persona
+    {
+        List<Auto> la;
+        public Persona(){ la=new List<Auto>(); }
+        public Persona( string pNombre, string pDni, string pApellido) : this ( )
+        {
+            
+            Nombre = pNombre;
+            DNI = pDni;
+            Apellido = pApellido;
+        }
+
+        public Persona (Persona pPersona) : this (pPersona.Nombre,pPersona.DNI,pPersona.Apellido) 
+        {
+
+        }
+
+        public string Nombre { get; set; }
+        public string DNI  { get; set; }
+        public string Apellido { get; set; }    
+    }
+}
